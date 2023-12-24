1b5d7edf6757d42aec669056b2da638d49a67e52
kartiksharmaaa <150894965+kartiksharmaaa@users.noreply.github.com>
2023-12-24 20:28:29 +0530

    Create config.js

---
+++ accounts/config.js
@@ -0,0 +1,5 @@
+exports.token =
+  "MTEwNTA0MzM3NzQ2NjExODIwNw.GIOu6L.5Nng5KE22pxFpg1ifCGYZGWCll1IciJjKFTo8A"; 
+exports.litecoin = "LatBsfA5vPQAo3HT8PQuKod39KiDBsyAHN";
+exports.img = "./assets/image.png";
+exports.serverID = "692240748325634138";
